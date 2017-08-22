52f902ad2d8881bace8b681db1558fe509e6ab4a
mrhop <233402923@qq.com>
2017-08-22 13:15:30 +0800

    for user client

--- src/components/user/modulelist/ModuleList.vue
+++ src/components/user/modulelist/ModuleList.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="app-list">
+  <div class="module-list">
     <panel>
-      <h1 slot="header">应用列表</h1>
-      <vtable id="app-list" :editable="true" :actionUrls="actionUrls" :actions="actions"></vtable>
+      <h1 slot="header">模块列表</h1>
+      <vtable id="module-list" :editable="true" :actionUrls="actionUrls" :actions="actions"></vtable>
     </panel>
   </div>
 </template>
@@ -15,14 +15,18 @@
   let panel = huodhVuePlugins.panel
   let tab = huodhVuePlugins.tab
   let vtable = huodhVuePlugins.vtable
+  let ruleChangeConfig = {
+    url: commonUrls.moduleFormRuleUpdate,
+    method: 'post'
+  }
   export default {
     data () {
       return {
         actionUrls: {
-          addUrl: commonUrls.vuerouter.addapp,
-          detailUrl: commonUrls.vuerouter.appdetail,
-          infoUrl: commonUrls.vuerouter.editapp,
-          deleteUrl: commonUrls.deleteApp
+          addUrl: commonUrls.vuerouter.addmodule,
+          detailUrl: commonUrls.vuerouter.moduledetail,
+          infoUrl: commonUrls.vuerouter.editmodule,
+          deleteUrl: commonUrls.deleteModule
         },
         actions: {
           list: function (args) {
@@ -30,81 +34,145 @@
             var init = args.init
             var filters = args.filters
             var sorts = args.sorts
+            let initConfig = {
+              url: commonUrls.moduleList,
+              method: 'post',
+              data: {pager, filters, sorts, init: true}
+            }
             if (init) {
-              let config = {
-                url: commonUrls.appList,
-                method: 'post',
-                data: {pager, filters, sorts, init: true}
-              }
-              axios.request(config).then(function (response) {
-                global.store.commit('TABLE_SUCCESS', {
-                  id: 'app-list',
-                  data: {
-                    'rules': {
-                      'header': [
-                        {
-                          'name': '#sn',
-                          'title': '#sn'
+              axios.request(initConfig).then(function (response) {
+                delete ruleChangeConfig.data
+                axios.request(ruleChangeConfig).then(function (responseInner) {
+                  global.store.commit('TABLE_SUCCESS', {
+                    id: 'module-list',
+                    data: {
+                      'rules': {
+                        'header': [
+                          {
+                            'name': '#sn',
+                            'title': '#sn'
+                          },
+                          {
+                            'name': 'moduleName',
+                            'title': '模块名称',
+                            'type': 'text',
+                            'filter': true
+                          },
+                          {
+                            'name': 'activated',
+                            'title': '已激活',
+                            'type': 'select',
+                            'filter': true,
+                            editable: true,
+                            'items': [
+                              {
+                                'label': '是',
+                                'value': true
+                              },
+                              {
+                                'label': '否',
+                                'value': false
+                              }
+                            ]
+                          },
+                          {
+                            'name': 'clientId',
+                            'title': '所属客户端',
+                            'type': 'select',
+                            'filter': true,
+                            items: responseInner.data.clientIds,
+                            'ruleChange': true
+                          },
+                          {
+                            'name': 'parentId',
+                            'title': '父模块',
+                            'type': 'tree',
+                            'filter': true,
+                            literal: true
+                          },
+                          {
+                            'name': 'authorityId',
+                            'title': '模块权限',
+                            'type': 'select',
+                            'filter': true,
+                            literal: true
+                          }
+                        ],
+                        'action': {
+                          'add': true,
+                          'detail': true,
+                          'update': true,
+                          'delete': true
                         },
-                        {
-                          'name': 'clientName',
-                          'title': '应用名称',
-                          'type': 'text',
-                          'filter': true
-                        },
-                        {
-                          'name': 'clientId',
-                          'title': '应用账号',
-                          'type': 'text',
+                        'feature': {
                           'filter': true,
-                          'sortable': true
-                        },
-                        {
-                          'name': 'authorizedGrantTypesStr',
-                          'title': '授权方式',
-                          'type': 'text'
-                        },
-                        {
-                          'name': 'clientResourceScopesStr',
-                          'title': '授权范围',
-                          'type': 'text'
+                          'pager': true
                         }
-                      ],
-                      'action': {
-                        'add': true,
-                        'detail': true,
-                        'update': true,
-                        'delete': true
                       },
-                      'feature': {
-                        'filter': true,
-                        'pager': true
-                      }
+                      'data': response.data
                     },
-                    'data': response.data
-                  },
-                  callParameters: {pager, init: true}
+                    callParameters: {pager, init: true}
+                  })
+                }).catch(function (error) {
+                  global.store.commit('TABLE_FAILURE', {id: 'module-list', error})
                 })
               }).catch(function (error) {
-                global.store.commit('TABLE_FAILURE', {id: 'app-list', error})
+                global.store.commit('TABLE_FAILURE', {id: 'module-list', error})
               })
             } else {
               let config = {
-                url: commonUrls.appList,
+                url: commonUrls.moduleList,
                 method: 'post',
                 data: {pager, filters, sorts}
               }
               axios.request(config).then(function (response) {
                 global.store.commit('TABLE_SUCCESS', {
-                  id: 'app-list',
+                  id: 'module-list',
                   data: {
                     'data': response.data
                   },
                   callParameters: {pager, filters, sorts}
                 })
               }).catch(function (error) {
-                global.store.commit('TABLE_FAILURE', {id: 'app-list', error})
+                global.store.commit('TABLE_FAILURE', {id: 'module-list', error})
               })
+            }
+          },
+          ruleChange: function (params) {
+            if (params.changed.hasOwnProperty('clientId')) {
+              if (params.changed.clientId !== undefined) {
+                ruleChangeConfig.data = params.changed
+                axios.request(ruleChangeConfig).then(function (response) {
+                  global.store.commit('TABLE_RULE_CHANGE_SUCCESS', {
+                    id: 'module-list',
+                    data: [
+                      {
+                        'name': 'parentId',
+                        'treeData': response.data.parentTree || []
+                      }, {
+                        'name': 'authorityId',
+                        'items': response.data.authorityIds || []
+                      }
+                    ]
+                  })
+                }).catch(function (error) {
+                  global.store.commit('TABLE_RULE_CHANGE_FAILURE', {
+                    id: 'module-list',
+                    error
+                  })
+                })
+              } else {
+                return [
+                  {
+                    'name': 'parentId',
+                    'treeData': []
+                  },
+                  {
+                    'name': 'authorityId',
+                    'items': []
+                  }
+                ]
+              }
             }
           }
         }
